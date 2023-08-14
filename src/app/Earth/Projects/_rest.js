d474f088c98ee1085fde1c40b04a67ae79a09658
Mark Joseph Salem <125126248+MarkSalem804@users.noreply.github.com>
2023-08-14 23:49:12 +0800

    added projects submodule

---
+++ src/app/Earth/Projects/_rest.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const auth = require("../../../middlewares/auth");
+const db = require("../../../middlewares/db");
+const schema = require("../../../middlewares/schema");
+const asyncHandler = require("express-async-handler");
+const Service = require("./project-service");
+const uploadFile = require("../../../middlewares/upload-file");
+
+const service = new Service();
+const router = express.Router();
+
+//Add / Import
+router.post("/project", db, uploadFile, asyncHandler(service.add));
+
+//Get
+router.get("/project/get/:uuid", auth, db, asyncHandler(service.get));
+
+//Graph & Table
+// router.get("/nursery/data", db, asyncHandler(service.getData));
+
+//Update
+router.put("/project/update/:uuid", db, asyncHandler(service.update));
+
+//Delete
+router.delete("/project/delete/:uuid", db, asyncHandler(service.delete));
+
+//Export
+// router.get("/project/export", db, asyncHandler(service.exportDataToExcel));
+
+module.exports = router;
---
+++ src/configuration/earthConfig/projectsTableConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+    tableName: 'projects',
+    columnNames: {
+      id: 'uuid',
+      nurseries: 'start_date',
+      reportDate: 'project_name',
+      fundedBy: 'end_Date',
+      region: 'cost',
+    },
+  };
